c53109b9cb9bf9159cdbdefd207b298c69b9e60a
zemfira1 <craftyzema@gmail.com>
2023-03-19 19:21:54 -0400

    +modal buy now js

---
+++ src/buy.js
@@ -0,0 +1,15 @@
+(() => {
+  const refs = {
+    openBuyBtn: document.querySelector("[data-buy-open]"),
+    closeBuyBtn: document.querySelector("[data-buy-close]"),
+    buy: document.querySelector("[data-buy]"),
+  };
+
+  refs.openBuyBtn.addEventListener("click", toggleBuy);
+  refs.closeBuyBtn.addEventListener("click", toggleBuy);
+
+  function toggleBuy() {
+    refs.buy.classList.toggle("visually-hidden");
+    document.body.classList.toggle("no-scroll")
+  }
+})();
