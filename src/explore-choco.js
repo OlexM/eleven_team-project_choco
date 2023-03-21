b66e41e7fa9e8f3e3ea4c2d5814f7debbfccf272
OlexM <mxalex32@gmail.com>
2023-03-22 00:18:10 +0200

    fix-modal

--- src/explore-choco.js
+++ src/explore-choco.js
@@ -3,10 +3,17 @@
     openReviewBtn: document.querySelector("[data-explore-open]"),
     closeReviewBtn: document.querySelector("[data-explore-close]"),
     review: document.querySelector("[data-explore]"),
+    closeSubmitBtn: document.querySelector(".new-chocolate-btn-submit"),
+    
+
   };
 
   refs.openReviewBtn.addEventListener("click", toggleReview);
   refs.closeReviewBtn.addEventListener("click", toggleReview);
+  refs.closeSubmitBtn.addEventListener("click", toggleReview);
+  
+
+
 
   function toggleReview() {
     refs.review.classList.toggle("is-hidden");
--- src/review.js
+++ src/review.js
@@ -3,10 +3,12 @@
     openReviewBtn: document.querySelector("[data-review-open]"),
     closeReviewBtn: document.querySelector("[data-review-close]"),
     review: document.querySelector("[data-review]"),
+    closeSendBtn: document.querySelector("[data-send-close]"),
   };
 
   refs.openReviewBtn.addEventListener("click", toggleReview);
   refs.closeReviewBtn.addEventListener("click", toggleReview);
+  refs.closeSendBtn.addEventListener("click", toggleReview);
 
   function toggleReview() {
     refs.review.classList.toggle("is-hidden");
